d7c5c48179b4548e44510ef6a935d4f7cdb9471a
Andrey Salomatin <filipovskii.off@gmail.com>
2024-10-14 00:22:51 +0200

    wip

--- karkar-web/app/root.tsx
+++ karkar-web/app/root.tsx
@@ -91,7 +91,22 @@ function isRouteMatch(routePath: string, routeMatches: UIMatch[]): boolean {
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  const { userId } = useLoaderData<typeof loader>()
+  const data = useLoaderData<typeof loader>()
+  const userId = data?.userId
+
+  if (!userId) {
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Meta />
+          <Links />
+        </head>
+        <body>No userId</body>
+      </html>
+    )
+  }
   const actionData = useActionData<typeof action>()
   const matches = useMatches()
 
